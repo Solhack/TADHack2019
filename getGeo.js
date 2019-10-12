f81952f8e564ba6977abd2e91c7436c13921bdb8
gift <you@example.com>
2019-10-12 19:59:09 +0200

    get geo js script

--- getGeo.js
+++ getGeo.js
@@ -1,30 +1,37 @@
-/*
- * This is a JavaScript Scratchpad.
- *
- * Enter some JavaScript, then Right Click or choose from the Execute Menu:
- * 1. Run to evaluate the selected text (Ctrl+R),
- * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
- * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
- */
+/* 
+  getGeo using location IQ
+  getGeoHERE using here maps
 
-function getGeo(lat, lon){
+  ## here maps is less accurate but use it for sake of project
+  
+  //all requests are simple ajax requests
+  
+  ASYNC so place callback function which will receive a parsed JSON object
+  
+  E.G getGeoHERE('-26.150353,27.932031' ,function(obj){ console.log(obj) } )
+  
+  getGeo(-26.150353, 27.932031, function(obj){ console.log(obj) } )
+  
+*/
+
+function getGeo(lat, lon , callback){
   var statusR = new XMLHttpRequest();
   statusR.onreadystatechange = function() {
     console.log(statusR.response)
     if (statusR.readyState == 4 && statusR.responseText != 'error') {
-
+      callback(JSON.parse(statusR.response))
     }
   }
   statusR.open("GET", 'https://us1.locationiq.com/v1/reverse.php?key=cbe1019cc2965e&lat='+lat+'&lon='+lon+'&format=json', true);
   statusR.send(null);  
 }
 
-function getGeoHERE(latlon){
+function getGeoHERE(latlon, callback){
   var statusR = new XMLHttpRequest();
   statusR.onreadystatechange = function() {
     console.log(statusR.response)
     if (statusR.readyState == 4 && statusR.responseText != 'error') {
-
+      callback(JSON.parse(statusR.response))
     }
   }
   statusR.open("GET", 'https://reverse.geocoder.api.here.com/6.2/reversegeocode.json?prox='+latlon+'&mode=retrieveAddresses&maxresults=1&gen=9&app_id=B4reavFnn0uYvunnrT5F&app_code=nsgciwl5LqSQPG0AbV0meg', true);
